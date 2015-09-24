e6cd45c3506ff5402f5d71bff20818e1bacbdadd
Mark Mandel <mark.mandel@gmail.com>
2015-09-23 20:28:41 -0700

    Initial commit of the template.

---
+++ src/template/template.go
@@ -0,0 +1,6 @@
+package template
+
+//Hello says hello
+func Hello() string {
+	return "Hello!"
+}
---
+++ src/template/template_test.go
@@ -0,0 +1,7 @@
+package template
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Fatal("Oh noes. I failed.")
+}
