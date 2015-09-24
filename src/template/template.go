c4e28cb025cb36deb4f4e92fba400bc750b16d5b
Mark Mandel <mark.mandel@gmail.com>
2015-09-24 07:55:01 -0700

    First Dojo!

--- src/template/template.go
+++ src/template/template.go
@@ -4,3 +4,27 @@ package template
 func Hello() string {
 	return "Hello!"
 }
+
+type RomanNumeral string
+
+// Adds roman numerals together (Ex. I + III = IV)
+func AddRoman(s RomanNumeral, t RomanNumeral) RomanNumeral {
+	IsByLiteral := map[RomanNumeral]RomanNumeral{
+		"I":"I",
+		"II":"II",
+		"III":"III",
+		"IV":"IIII",
+		"V":"IIIII",
+		"VI":"IIIIII",
+	}
+	LiteralsByI := map[RomanNumeral]RomanNumeral{
+		"IIIIII":"VI",
+		"IIIII":"V",
+		"IIII":"IV",
+		"III":"III",
+		"II":"II",
+		"I":"I",
+	}
+    Is := IsByLiteral[s] + IsByLiteral[t]
+	return LiteralsByI[Is]
+}
--- src/template/template_test.go
+++ src/template/template_test.go
@@ -3,5 +3,43 @@ package template
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Fatal("Oh noes. I failed.")
+	if Hello() != "Hello!" {
+		t.Fatal("Didn't return the right string.")
+	}
 }
+
+func TestAddRomanIs(t *testing.T) {
+	if AddRoman("I", "I") != "II" {
+		t.Fatal("I+I should == II")
+	}
+
+	if AddRoman("I", "II") != "III" {
+		t.Fatal("I + II should == III")
+	}
+}
+
+func TestAddRomanV(t *testing.T) {
+	if AddRoman("I", "III") != "IV" {
+		t.Fatal("I+III should == IV")
+	}
+
+	if AddRoman("III", "III") != "VI" {
+		t.Fatal("III+III should == VI")
+	}
+
+	if AddRoman("IV", "I") != "V" {
+		t.Fatal("IV+I should == V")
+	}
+
+	if AddRoman("V", "I") != "VI" {
+		t.Fatal("V+I should == VI")
+	}
+}
+
+//func TestAddRomanIVPlusVI(t *testing.T) {
+//	if AddRoman("IV", "VI") != "X" {
+//		t.Fatal("IV+VI should == X")
+//	}
+//}
+
+
