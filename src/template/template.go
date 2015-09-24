ea6695a4abf0a851c44b0f8b158dc02e8c254d56
Mark Mandel <mark.mandel@gmail.com>
2015-09-24 08:36:33 -0700

    add validation

--- src/template/template.go
+++ src/template/template.go
@@ -1,4 +1,5 @@
 package template
+import "errors"
 
 //Hello says hello
 func Hello() string {
@@ -7,10 +8,18 @@ func Hello() string {
 
 type RomanNumeral string
 
+func (r RomanNumeral) Valid() bool {
+	_, ok := isByLiteral[r]
+	return ok
+}
+
 // Adds roman numerals together (Ex. I + III = IV)
-func (r RomanNumeral) Add(s RomanNumeral) RomanNumeral {
-	Is := isByLiteral[r] + isByLiteral[s]
-	return literalsByI[Is]
+func (r RomanNumeral) Add(s RomanNumeral) (RomanNumeral, error) {
+	if s.Valid() && r.Valid() {
+		is := isByLiteral[r] + isByLiteral[s]
+		return literalsByI[is], nil
+	}
+	return "", errors.New("One of these is not a roman numeral")
 }
 
 var isByLiteral = map[RomanNumeral]RomanNumeral{
@@ -20,13 +29,15 @@ var isByLiteral = map[RomanNumeral]RomanNumeral{
 	"IV":  "IIII",
 	"V":   "IIIII",
 	"VI":  "IIIIII",
+	"X":   "IIIIIIIIII",
 }
 
 var literalsByI = map[RomanNumeral]RomanNumeral{
-	"IIIIII": "VI",
-	"IIIII":  "V",
-	"IIII":   "IV",
-	"III":    "III",
-	"II":     "II",
-	"I":      "I",
+	"IIIIIIIIII": "X",
+	"IIIIII":     "VI",
+	"IIIII":      "V",
+	"IIII":       "IV",
+	"III":        "III",
+	"II":         "II",
+	"I":          "I",
 }
--- src/template/template_test.go
+++ src/template/template_test.go
@@ -9,37 +9,51 @@ func TestHello(t *testing.T) {
 }
 
 func TestAddRomanIs(t *testing.T) {
-	if RomanNumeral("I").Add("I") != "II" {
+	added, _ := RomanNumeral("I").Add("I")
+	if added != RomanNumeral("II") {
 		t.Fatal("I+I should == II")
 	}
 
-	if RomanNumeral("I").Add("II") != "III" {
+	added3, _ := RomanNumeral("I").Add("II")
+	if added3 != "III" {
 		t.Fatal("I + II should == III")
 	}
 }
 
 func TestAddRomanV(t *testing.T) {
-	if RomanNumeral("I").Add("III") != "IV" {
+	added4, _ := RomanNumeral("I").Add("III")
+	if added4 != "IV" {
 		t.Fatal("I+III should == IV")
 	}
 
-	if RomanNumeral("III").Add("III") != "VI" {
+	added5, _ := RomanNumeral("III").Add("III")
+	if added5 != "VI" {
 		t.Fatal("III+III should == VI")
 	}
 
-	if RomanNumeral("IV").Add("I") != "V" {
+	added6, _ := RomanNumeral("IV").Add("I")
+	if added6 != "V" {
 		t.Fatal("IV+I should == V")
 	}
 
-	if RomanNumeral("V").Add("I") != "VI" {
+	added7, _ := RomanNumeral("V").Add("I")
+	if added7 != "VI" {
 		t.Fatal("V+I should == VI")
 	}
 }
 
-//func TestAddRomanIVPlusVI(t *testing.T) {
-//	if RomanNumeral("IV").Add("VI") != "X" {
-//		t.Fatal("IV+VI should == X")
-//	}
-//}
+func TestAddRomanIVPlusVI(t *testing.T) {
+	added8, _ := RomanNumeral("IV").Add("VI")
+	if added8 != "X" {
+		t.Fatal("IV+VI should == X")
+	}
+}
+
+func TestFailIfNotRomanNumeral(t *testing.T) {
+	_, err := RomanNumeral("Q").Add("I")
+	if err == nil {
+		t.Fatal("Q + I should throw error")
+	}
+}
 
 
